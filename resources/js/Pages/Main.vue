bbebb6ec495038efbbf84f2a79a6572574df4e77
maksym <riabchuk.maksym@gmail.com>
2025-03-16 19:10:47 +0100

    BS-1 Start

---
+++ resources/js/Pages/Main.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref, reactive, onMounted} from "vue";
+const props = defineProps({
+    data:String,
+});
+onMounted(() => {
+    console.log(props);
+})
+</script>
+
+<template>
+    Main page of the site
+</template>
+
+<style scoped>
+
+</style>
